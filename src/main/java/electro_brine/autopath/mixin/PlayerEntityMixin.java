12a9bcc244578bab3dc5b417d0ae9fc1f688f366
ElectroBrine <darienpiva@gmail.com>
2021-09-01 22:34:41 -0600

    Paths no longer generate in claims

--- src/main/java/electro_brine/autopath/mixin/PlayerEntityMixin.java
+++ src/main/java/electro_brine/autopath/mixin/PlayerEntityMixin.java
@@ -1,6 +1,11 @@
 
 package electro_brine.autopath.mixin;
 
+import mrnavastar.kingdoms.api.interfaces.PlayerEntityInf;
+import mrnavastar.kingdoms.types.Claim;
+import mrnavastar.kingdoms.types.Kingdom;
+import mrnavastar.kingdoms.util.AdminClaimManager;
+import mrnavastar.kingdoms.util.KingdomsManager;
 import net.minecraft.block.*;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
@@ -8,6 +13,7 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,14 +21,42 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.ArrayList;
+
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity {
 
     private BlockPos playerPos = this.getBlockPos();
 
-
     protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
         super(entityType, world);
+    }
+
+
+    public boolean isInClaim(BlockPos pos) {
+        if (world.getRegistryKey().getValue().toString().equals("minecraft:overworld")) {
+            for (Kingdom kingdom : KingdomsManager.getKingdoms()) {
+                for (Claim claim : kingdom.getOverworldClaims()) {
+                    return claim.isInClaim(pos);
+                }
+            }
+            for (Claim claim : AdminClaimManager.overworldClaims) {
+                return claim.isInClaim(pos);
+            }
+        }
+
+        if (world.getRegistryKey().getValue().toString().equals("minecraft:the_nether")) {
+            for (Kingdom kingdom : KingdomsManager.getKingdoms()) {
+                for (Claim claim : kingdom.getNetherClaims()) {
+                    return claim.isInClaim(pos);
+                }
+            }
+            for (Claim claim : AdminClaimManager.netherClaims) {
+                return claim.isInClaim(pos);
+            }
+        }
+
+        return false;
     }
 
     public void Block() {
@@ -31,7 +65,12 @@ public abstract class PlayerEntityMixin extends LivingEntity {
         BlockPos blockPos = new BlockPos( this.getBlockPos().getX(), this.getBlockPos().getY() - .5, this.getBlockPos().getZ());
         Block block = world.getBlockState(blockPos).getBlock();
 
-        if (Math.random()*(100) <= 10 && !this.isSneaking() && !this.getStatusEffects().contains(new StatusEffectInstance(StatusEffects.SLOW_FALLING)) && !this.getStatusEffects().contains(new StatusEffectInstance(StatusEffects.INVISIBILITY)) && playerBlock != Blocks.WATER && playerBlock != Blocks.BAMBOO && playerBlock != Blocks.SUGAR_CANE && playerBlock != Blocks.NETHER_WART) {
+        boolean pathCreate = Math.random()*(100) <= 5 && !isInClaim(blockPos) && !this.isSneaking() && !this.getStatusEffects().contains(new StatusEffectInstance(StatusEffects.SLOW_FALLING)) && !this.getStatusEffects().contains(new StatusEffectInstance(StatusEffects.INVISIBILITY)) && playerBlock != Blocks.WATER && playerBlock != Blocks.BAMBOO && playerBlock != Blocks.SUGAR_CANE && playerBlock != Blocks.NETHER_WART && playerBlock != Blocks.SWEET_BERRY_BUSH;
+        boolean gravelChance = Math.random()*(100) <= 10;
+        boolean glassChance = Math.random() * (1000) <= 1;
+        boolean roadDecay = Math.random()*(1000)<=1;
+
+        if (pathCreate) {
             if (playerBlock == Blocks.FERN
                     || playerBlock == Blocks.LARGE_FERN
                     || playerBlock == Blocks.GRASS
@@ -60,73 +99,67 @@ public abstract class PlayerEntityMixin extends LivingEntity {
                     world.setBlockState(blockPos, Blocks.COARSE_DIRT.getDefaultState());
                 }
                 if (block == Blocks.COARSE_DIRT) {
-                    if (Math.random() * (100) >= 75) {
+                    if (gravelChance) {
                         world.setBlockState(blockPos, Blocks.GRAVEL.getDefaultState());
                     } else {
                         world.setBlockState(blockPos, Blocks.DIRT_PATH.getDefaultState());
                     }
                 }
                 if (block == Blocks.GRAVEL) {
-                    if (Math.random()*(100+1) >= 25) {
-                        if (Math.random()*(100) <= 33) {
-                            world.setBlockState(blockPos, Blocks.COBBLESTONE.getDefaultState());
-                        }
-                        else if (Math.random()*(100+1) >= 33){
-                            world.setBlockState(blockPos, Blocks.TUFF.getDefaultState());
-                        }
-                        else {
-                            world.setBlockState(blockPos, Blocks.MOSSY_COBBLESTONE.getDefaultState());
-                        }
-                    }
-                    else{
+                    double chance = Math.random() * (100);
+                    if (chance >= 75) {
+                        world.setBlockState(blockPos, Blocks.COBBLESTONE.getDefaultState());
+                    } else if (chance >= 50) {
+                        world.setBlockState(blockPos, Blocks.TUFF.getDefaultState());
+                    } else if (chance >= 25) {
+                        world.setBlockState(blockPos, Blocks.MOSSY_COBBLESTONE.getDefaultState());
+                    } else {
                         world.setBlockState(blockPos, Blocks.ANDESITE.getDefaultState());
                     }
-
                 }
                 if (block == Blocks.SAND) {
-                    if (Math.random() * (1000) <= 2) {
+                    if (glassChance) {
                         world.setBlockState(blockPos, Blocks.GLASS.getDefaultState());
-                    }
-                    else {
+                    } else {
                         world.setBlockState(blockPos, Blocks.SANDSTONE.getDefaultState());
                     }
                 }
                 if (block == Blocks.RED_SAND) {
-                    if (Math.random() * (1000) <= 2) {
+                    if (glassChance) {
                         world.setBlockState(blockPos, Blocks.RED_STAINED_GLASS.getDefaultState());
-                    }
-                    else {
+                    } else {
                         world.setBlockState(blockPos, Blocks.RED_SANDSTONE.getDefaultState());
                     }
                 }
                 if (block == Blocks.SOUL_SAND) {
                     world.setBlockState(blockPos, Blocks.SOUL_SOIL.getDefaultState());
                 }
-                if (block == Blocks.WARPED_NYLIUM){
+                if (block == Blocks.WARPED_NYLIUM) {
                     world.setBlockState(blockPos, Blocks.NETHERRACK.getDefaultState());
                 }
-                if (block == Blocks.CRIMSON_NYLIUM){
+                if (block == Blocks.CRIMSON_NYLIUM) {
                     world.setBlockState(blockPos, Blocks.NETHERRACK.getDefaultState());
                 }
-                if (block == Blocks.STONE) {
-                    if (Math.random()*(1000)<1) {
-                        world.setBlockState(blockPos, Blocks.COBBLESTONE.getDefaultState());
-                    }
+                if (block == Blocks.STONE && roadDecay) {
+                    world.setBlockState(blockPos, Blocks.COBBLESTONE.getDefaultState());
                 }
-                if (block == Blocks.DEEPSLATE) {
-                    if (Math.random()*(1000)<1) {
-                        world.setBlockState(blockPos, Blocks.COBBLED_DEEPSLATE.getDefaultState());
-                    }
+                if (block == Blocks.DEEPSLATE && roadDecay) {
+                    world.setBlockState(blockPos, Blocks.COBBLED_DEEPSLATE.getDefaultState());
                 }
-                if (block == Blocks.STONE_BRICKS) {
-                    if (Math.random()*(10000)<1) {
-                        world.setBlockState(blockPos, Blocks.CRACKED_STONE_BRICKS.getDefaultState());
-                    }
+                if (block == Blocks.STONE_BRICKS && roadDecay) {
+                    world.setBlockState(blockPos, Blocks.CRACKED_STONE_BRICKS.getDefaultState());
                 }
-                if (block == Blocks.DEEPSLATE_BRICKS) {
-                    if (Math.random()*(10000)<1) {
-                        world.setBlockState(blockPos, Blocks.CRACKED_DEEPSLATE_BRICKS.getDefaultState());
-                    }
+                if (block == Blocks.CRACKED_STONE_BRICKS && roadDecay) {
+                    world.setBlockState(blockPos, Blocks.COBBLESTONE.getDefaultState());
+                }
+                if ((block == Blocks.DEEPSLATE_BRICKS || block == Blocks.CRACKED_DEEPSLATE_TILES) && roadDecay) {
+                    world.setBlockState(blockPos, Blocks.CRACKED_DEEPSLATE_BRICKS.getDefaultState());
+                }
+                if (block == Blocks.CRACKED_DEEPSLATE_BRICKS && roadDecay) {
+                    world.setBlockState(blockPos, Blocks.COBBLED_DEEPSLATE.getDefaultState());
+                }
+                if (block == Blocks.DEEPSLATE_TILES && roadDecay) {
+                    world.setBlockState(blockPos, Blocks.CRACKED_DEEPSLATE_TILES.getDefaultState());
                 }
             }
         }
